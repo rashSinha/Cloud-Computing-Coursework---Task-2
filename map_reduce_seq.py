65a5a277b6014740824789bba649580dc1b59876
Rashmil Sinha <rashmil.sinha@student.reading.ac.uk>
2023-04-16 16:33:58 +0000

    Sequential implementation

---
+++ map_reduce_seq.py
@@ -0,0 +1,32 @@
+import csv
+import pandas as pd
+
+def mapper(record):
+    passenger_id = record[0]
+    yield (passenger_id, 1)
+
+def reducer(key, values):
+    total_flights = sum(values)
+    yield (key, total_flights)
+
+def run_sequential():
+    # read csv file and create a list of tuples
+    with open('AComp_Passenger_data_no_error(1).csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader) # skip header row
+        data = [(row[0],) for row in reader]
+
+    # run sequential implementation with shuffler here
+    results_seq = {}
+    for record in data:
+        for key, value in mapper(record):
+            if key not in results_seq:
+                results_seq[key] = []
+            results_seq[key].append(value)
+
+    output_seq = [(key, sum(values)) for key, values in results_seq.items()]
+
+    # save sequential output to csv file
+    df_seq = pd.DataFrame(output_seq, columns=['Passenger ID', 'Number of Flights'])
+    df_seq = df_seq.sort_values(by='Number of Flights', ascending=False)
+    df_seq.to_csv('output_seq.csv', index=False)
