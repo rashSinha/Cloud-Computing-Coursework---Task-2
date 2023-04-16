666287b5a3b09eedfed3127d065a5ac3a9753884
Rashmil Sinha <rashmil.sinha@student.reading.ac.uk>
2023-04-16 21:31:02 +0000

    Update map_reduce_seq.py

--- map_reduce_seq.py
+++ map_reduce_seq.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 
 # Define mapper and reducer functions 
@@ -11,15 +10,12 @@ def reducer(key, values):
     yield (key, total_flights)
 
 def run_sequential():
-    # Read the csv file and create a list of tuples
-    with open('AComp_Passenger_data_no_error(1).csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader) # skip header row
-        data = [(row[0],) for row in reader]
+    # Read the csv file and create a pandas dataframe
+    df = pd.read_csv('AComp_Passenger_data_no_error(1).csv', header=None)
 
     # Run sequential implementation with shuffler here
     results_seq = {}
-    for record in data:
+    for record in df.itertuples(index=False):
         for key, value in mapper(record):
             if key not in results_seq:
                 results_seq[key] = []
@@ -31,3 +27,9 @@ def run_sequential():
     df_seq = pd.DataFrame(output_seq, columns=['Passenger ID', 'Number of Flights'])
     df_seq = df_seq.sort_values(by='Number of Flights', ascending=False)
     df_seq.to_csv('output_seq.csv', index=False)
+
+    # Print passenger with the highest number of flights
+    print(f"Passenger ID {df_seq.iloc[0]['Passenger ID']} has travelled the most with {df_seq.iloc[0]['Number of Flights']} flights.")
+
+if __name__ == '__main__':
+    run_sequential()
