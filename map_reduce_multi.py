6fc664d92fcaf8242ef1abf17952f0816a0076cf
Rashmil Sinha <rashmil.sinha@student.reading.ac.uk>
2023-04-17 00:14:32 +0000

    Updated code to be modular and encapsulated the mapper, shuffler and reducer functions into separate methods

--- map_reduce_multi.py
+++ map_reduce_multi.py
@@ -2,47 +2,68 @@ import pandas as pd
 import multiprocessing as mp
 from functools import reduce
 
-# Define Mapper function
-def mapper(x):
-    return (x, 1)
+'''Define separate templates for mapper, shuffler and reducer 
+to be called in a separate class PassengerDataAnalyzer'''
 
-# Define Shuffle function
-def shuffle(mapper_out):
-    data = {}
-    for k, v in mapper_out:
-        if k not in data:
-            data[k] = [v]
-        else:
-            data[k].append(v)
-    return data
+class Mapper:
+    def __init__(self, data):
+        self.data = data
+        
+    def __call__(self, x):
+        return (x, 1)
+    
 
-# Define Reduce function
-def reducer(x, y):
-    return x + y
+class Shuffler:
+    def __call__(self, mapper_out):
+        data = {}
+        for k, v in mapper_out:
+            if k not in data:
+                data[k] = [v]
+            else:
+                data[k].append(v)
+        return data
+    
+
+class Reducer:
+    def __call__(self, x, y):
+        return x + y
+    
+'''Define a class that can later be used to create different 
+instances of the passenger data for multiple files'''
+
+class PassengerDataAnalyzer:
+    def __init__(self, input_file_path):
+        self.input_file_path = input_file_path
+        self.mapper = Mapper(self._read_data())
+        self.shuffler = Shuffler()
+        self.reducer = Reducer()
+        
+    def _read_data(self):
+        return pd.read_csv(self.input_file_path, header=None).iloc[:, 0].tolist()
+        
+    def analyze(self):
+        with mp.Pool(processes=mp.cpu_count()) as pool:
+            # Map phase
+            map_out = pool.map(self.mapper, self.mapper.data, chunksize=int(len(self.mapper.data)/mp.cpu_count()))
+            
+            # Shuffle phase
+            reduce_in = self.shuffler(map_out)
+            
+            # Reduce phase
+            reduce_out = {}
+            for key, values in reduce_in.items():
+                reduce_out[key] = reduce(self.reducer, values)
+            
+            # Convert output to pandas dataframe and sort in descending order
+            df = pd.DataFrame(list(reduce_out.items()), columns=['Passenger ID', 'Number of Flights'])
+            df = df.sort_values('Number of Flights', ascending=False)
+            
+            # Print passenger with the highest number of flights
+            print(f"Passenger ID {df.iloc[0]['Passenger ID']} has travelled the most with {df.iloc[0]['Number of Flights']} flights.")
+            
+            # Save output to CSV file
+            df.to_csv('output_mp.csv', index=False)
 
 if __name__ == '__main__':
-    # Read input CSV file
-    data = pd.read_csv('/Users/rashmilsinha/Downloads/Task 2/AComp_Passenger_data_no_error(1).csv', header=None)
-    map_in = data.iloc[:, 0].tolist()
-    
-    with mp.Pool(processes=mp.cpu_count()) as pool:
-        # Map phase
-        map_out = pool.map(mapper, map_in, chunksize=int(len(map_in)/mp.cpu_count()))
-        
-        # Shuffle phase
-        reduce_in = shuffle(map_out)
-        
-        # Reduce phase
-        reduce_out = {}
-        for key, values in reduce_in.items():
-            reduce_out[key] = reduce(reducer, values)
-        
-        # Convert output to pandas dataframe and sort in descending order
-        df = pd.DataFrame(list(reduce_out.items()), columns=['Passenger ID', 'Number of Flights'])
-        df = df.sort_values('Number of Flights', ascending=False)
-        
-        # Print passenger with the highest number of flights
-        print(f"Passenger ID {df.iloc[0]['Passenger ID']} has travelled the most with {df.iloc[0]['Number of Flights']} flights.")
-        
-        # Save output to CSV file
-        df.to_csv('output_mp.csv', index=False)
+    analyzer = PassengerDataAnalyzer('AComp_Passenger_data_no_error(1).csv')
+    analyzer.analyze()
